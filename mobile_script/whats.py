e887d29850dbbb8c0ccdd710527d24f81fa87f06
rantokl <lalainazomentsoa@gmail.com>
2023-09-08 07:16:51 +0300

    UPDATE

---
+++ mobile_script/whats.py
@@ -0,0 +1,20 @@
+import pywhatkit
+import time
+import random
+current_time = time.localtime()
+hour = current_time.tm_hour
+minute = current_time.tm_min + 1
+def sendsms(number, sms):
+    send = pywhatkit.sendwhatmsg_instantly(
+        number, 
+        sms,
+        15,
+        True,
+        5
+        
+    
+    )
+    if send:
+        print("Message send succeffully")
+    else:
+        print("Error, send message")
